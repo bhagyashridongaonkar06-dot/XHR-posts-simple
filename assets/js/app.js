e7d95cdf7143959db72ada742b2d50e83809a985
bhagyashridongaonkar06-dot <bhagyashridongaonkar06@gmail.com>
2026-09-13 22:55:16 +0530

    create functionality done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,10 @@
 const cl = console.log;
 
+const postForm = document.getElementById('postForm')
+const title = document.getElementById('title')
+const content = document.getElementById('content')
+const userId = document.getElementById('userId')
+
 
 let base_url = 'https://jsonplaceholder.typicode.com/'
 let post_url = `${base_url}/posts`
@@ -19,7 +24,7 @@ xhr.onload = function (){
     if(xhr.status >= 200 && xhr.status <= 299){
         let data = JSON.parse(xhr.response)
         let res = '';
-        data.forEach(ele => {
+        data.reverse().forEach(ele => {
             res += `<div class="col-3 mt-5" id=${ele.id}>
                 <div class="card h-100">
                     <div class="card-header">
@@ -40,3 +45,41 @@ xhr.onload = function (){
     }
 }
 
+
+function onSubmit(eve){
+    eve.preventDefault();
+
+    let newObj = {
+        title : title.value,
+        body : content.value,
+        userId : userId.value
+    }
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", post_url);
+    xhr.send(JSON.stringify(newObj));
+    xhr.onload = () => {
+        if(xhr.status === 201 && xhr.readyState == 4){
+            
+            let newPost = document.createElement("div");
+            newPost.className = "col-3 mt-5";
+            newPost.id = xhr.response.id
+            newPost.innerHTML = `<div class="card h-100">
+                    <div class="card-header">
+                        <h3 class="m-0">${newObj.title}</h3>
+                    </div>
+                    <div class="card-body">
+                        <p class="m-0">${newObj.body}</p>
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <button class="btn btn-sm btn-primary">Edit</button>
+                        <button class="btn btn-sm btn-danger">Remove</button>
+                    </div>
+                </div>`
+            postContainer.prepend(newPost)
+        }
+    }
+}
+
+
+postForm.addEventListener('submit', onSubmit)
