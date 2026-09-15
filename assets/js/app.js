06c53002b62b81623775ad91350aa2c68deddade
bhagyashridongaonkar06-dot <bhagyashridongaonkar06@gmail.com>
2026-09-15 15:57:21 +0530

    change in read function

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,7 @@ let xhr = new XMLHttpRequest();
 
 xhr.open('GET', post_url, true)
 
-xhr.send()
+xhr.send(null)
 
 xhr.onload = function (){
     cl(xhr.status)
@@ -25,7 +25,7 @@ xhr.onload = function (){
         let data = JSON.parse(xhr.response)
         let res = '';
         data.reverse().forEach(ele => {
-            res += `<div class="col-3 mt-5" id=${ele.id}>
+            res += `<div class="col-3 mt-5" id="${ele.id}">
                 <div class="card h-100">
                     <div class="card-header">
                         <h3 class="m-0">${ele.title}</h3>
@@ -64,7 +64,7 @@ function onSubmit(eve){
             
             let newPost = document.createElement("div");
             newPost.className = "col-3 mt-5";
-            newPost.id = xhr.response.id
+            newPost.id = res.id
             newPost.innerHTML = `<div class="card h-100">
                     <div class="card-header">
                         <h3 class="m-0">${newObj.title}</h3>
@@ -73,13 +73,23 @@ function onSubmit(eve){
                         <p class="m-0">${newObj.body}</p>
                     </div>
                     <div class="card-footer d-flex justify-content-between">
-                        <button class="btn btn-sm btn-primary">Edit</button>
-                        <button class="btn btn-sm btn-danger">Remove</button>
+                        <button onclick="onEdit(this)" class="btn btn-sm btn-primary">Edit</button>
+                        <button onclick="onDelete(this)" class="btn btn-sm btn-danger">Remove</button>
                     </div>
                 </div>`
             postContainer.prepend(newPost)
         }
     }
+}
+
+
+function onEdit(){
+    let editId = ele.closest('.col-3').id;
+    cl(editId)
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open()
 }
 
 
